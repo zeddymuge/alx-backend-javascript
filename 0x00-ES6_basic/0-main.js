8ea5e35c29fff54b99360844bbebb6c058f3d282
zeddy muge <cheroozeddy@gmail.com>
2023-04-17 13:59:09 +0000

    done

---
+++ 0x00-ES6_basic/0-main.js
@@ -0,0 +1,13 @@
+/*import taskBlock from './1-block-scoped.js';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
+
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);*/
+import concatArrays from './5-spread-operator.js';
+
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
---
+++ 0x00-ES6_basic/babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env'
+  ],
+  plugins: [
+    // Add any additional plugins you want to use
+  ]
+};
+
