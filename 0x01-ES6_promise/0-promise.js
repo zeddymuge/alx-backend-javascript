ae9e4b6f8cb9f9878bfdcac09c154813f4876053
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2023-04-19 14:26:56 +0300

    done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
-function getResponseFromAPI(){
-	return new promise((resolve, reject) =>{
-		//eslint-disable-next-line no-constant-condition
-		if(true){
-			resolve();
-		}else{
-			reject();
-		}
-	});
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-constant-condition
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
