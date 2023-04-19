034bd7f2c8acb8a1ad33f4b0b972ff2322a13db7
zeddymuge <cheroozeddy@gmail.com>
2023-04-19 11:20:24 +0000

    done

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+function getResponseFromAPI(){
+	return new promise((resolve, reject) =>{
+		//eslint-disable-next-line no-constant-condition
+		if(true){
+			resolve();
+		}else{
+			reject();
+		}
+	});
+}
