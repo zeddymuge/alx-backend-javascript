48ab113c7295d3db4caae7ee62e2089ffa1fd089
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2023-04-25 17:02:48 +0300

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,5 @@
-function getListStudents() {
+
+export default function getListStudents() {
   return [
     { id: 1, firstName: "Guillaume", location: "San Francisco" },
     { id: 2, firstName: "James", location: "Columbia" },
